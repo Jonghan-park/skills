4dda3614998ef46311f8428bbffeacb6906099db
jonghan-park <jonghan.park@edu.sait.ca>
2022-03-23 19:24:49 -0600

    Create data.js and Update Skills.js

---
+++ src/Skills.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import skills from './data';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+
+
+const Reviews = () => {
+    return <h1>my skills</h1>
+};
+
+export default Reviews;
