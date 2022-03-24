ab39816fc2893a9e4311846addf7cbc5780c893b
jonghan-park <jonghan.park@edu.sait.ca>
2022-03-23 19:39:48 -0600

    Update skill.js

--- src/Skills.js
+++ src/Skills.js
@@ -4,7 +4,38 @@ import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 
 const Reviews = () => {
-    return <h1>my skills</h1>
+    const [index, setIndex] = useState(0);
+    const {name, image, text} = skills[index];
+
+    const checkNumber = (number) =>{
+        if(number > skills.length - 1){
+            return 0;
+        }
+        if(number < 0){
+            return skills.length - 1;
+        }
+        return number;
+    };
+
+    const nextSkill = () => {
+        setIndex ((index) => {
+            let newIndex = index + 1;
+            return checkNumber(newIndex);
+        });
+    };
+
+    const prevSkill = () => {
+        setIndex ((index) => {
+            let newIndex = index - 1;
+            return checkNumber(newIndex);
+        });
+    };
+
+    return (
+        <article className='skill'>
+
+        </article>
+    );
 };
 
 export default Reviews;
