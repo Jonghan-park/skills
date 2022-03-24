06ffb424343ca9631b08858aac813ea337b8d015
jonghan-park <jonghan.park@edu.sait.ca>
2022-03-23 19:30:49 -0600

    Update data.js

---
+++ src/data.js
@@ -0,0 +1,36 @@
+const skills = [
+    {
+      id: 1,
+      name: 'HTML',
+      image:
+        'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F6%2F61%2FHTML5_logo_and_wordmark.svg%2F1200px-HTML5_logo_and_wordmark.svg.png&f=1&nofb=1',
+      text:
+        "The HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser.",
+    },
+    {
+      id: 2,
+      name: 'CSS',
+      image:
+        'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fdevlup.com%2Fwp-content%2Fuploads%2F2013%2F07%2Fcss-logo.jpg&f=1&nofb=1',
+      text:
+        'Cascading Style Sheets is a style sheet language used for describing the presentation of a document written in a markup language such as HTML. CSS is a cornerstone technology of the World Wide Web, alongside HTML and JavaScript.',
+    },
+    {
+      id: 3,
+      name: 'Javascript',
+      image:
+        'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.bookofnetwork.com%2Fimages%2Fcode%2Fimages%2Fjs.jpg&f=1&nofb=1',
+      text:
+        'Javascript is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS. Over 97% of websites use JavaScript on the client side for web page behavior, often incorporating third-party libraries.',
+    },
+    {
+      id: 4,
+      name: 'React.js',
+      image:
+        'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.ideematic.com%2Fwp-content%2Fuploads%2F2018%2F05%2Freact-JS-developpement-web.png&f=1&nofb=1',
+      text:
+        'React is a free and open-source front-end JavaScript library for building user interfaces based on UI components. It is maintained by Meta and a community of individual developers and companies. React can be used as a base in the development of single-page, mobile, or server-rendered applications with frameworks like Next.js.',
+    },
+  ];
+  
+  export default skills;
